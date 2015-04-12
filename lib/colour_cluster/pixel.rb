8a4b51e45255b3670868065472d4be1a91ea3e19
Hugh Brace <hughbrace@gmail.com>
2015-04-12 18:14:53 +0100

    Added Pixel class and tests

--- lib/colour_cluster/pixel.rb
+++ lib/colour_cluster/pixel.rb
@@ -1,3 +1,30 @@
 class Pixel
 
+  attr_reader :red, :green, :blue
+
+  def initialize(red, green, blue)
+    @red    = red
+    @green  = green
+    @blue   = blue
+  end
+
+  def distance_to(other_pixel)
+    sum = ((@red    - other_pixel.red)   ** 2) +
+          ((@green  - other_pixel.green) ** 2) +
+          ((@blue   - other_pixel.blue)  ** 2)
+
+    sum ** 0.5
+  end
+
+  def +(other_pixel)
+    Pixel.new(@red + other_pixel.red, @green + other_pixel.green, 
+              @blue + other_pixel.blue)
+  end
+
+  def /(n)
+    Pixel.new(@red / n, @green  / n, @blue / n)
+  end
+
 end
+
+
--- spec/pixel_spec.rb
+++ spec/pixel_spec.rb
@@ -2,5 +2,67 @@ require 'spec_helper'
 
 describe Pixel do
 
+  describe '.new(r, g, b)' do
+    before(:all) do
+      @red    = 100
+      @green  = 200
+      @blue   = 150
+
+      @p = Pixel.new(@red, @green, @blue)
+
+    end
+
+    it 'instantiates' do
+      expect(@p).to be_instance_of Pixel
+    end
+
+    it 'remembers its member variables' do
+      expect(@p.red).to   eq @red
+      expect(@p.green).to eq @green
+      expect(@p.blue).to  eq @blue
+    end
+  end
+
+  context 'pixel distance and addition' do
+
+    before(:all) do
+      @pixel_1 = Pixel.new(1, 2, 3)
+      @pixel_2 = Pixel.new(-4, -8, -12)
+    end
+
+    describe '.distance_to(pixel)' do
+      it 'can calculate distance from another pixel instance' do
+        expect(@pixel_1.distance_to(@pixel_2)).to eql 18.708286933869708
+        expect(@pixel_2.distance_to(@pixel_1)).to eql 18.708286933869708
+      end
+    end
+
+    describe '.+(pixel)' do
+      it 'implements the addition operator' do
+        pixel_temp = @pixel_1 + @pixel_2
+
+        expect(pixel_temp.red).to   eql -3
+        expect(pixel_temp.green).to eql -6
+        expect(pixel_temp.blue).to  eql -9
+      end
+    end
+
+    describe './(n)' do
+      it 'implements the division operator' do
+        pixel_temp = @pixel_1 / 2
+
+        expect(pixel_temp.red).to   eql 0
+        expect(pixel_temp.green).to eql 1
+        expect(pixel_temp.blue).to  eql 1
+
+        pixel_temp = @pixel_2 / 3
+
+        expect(pixel_temp.red).to   eql -2
+        expect(pixel_temp.green).to eql -3
+        expect(pixel_temp.blue).to  eql -4
+      end
+    end
+
+  end
 
 end
